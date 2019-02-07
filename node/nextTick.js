f6c02673b31afe44dacd49044a4b0f16df4a64d2
jsunga <jawyn.sunga@gmail.com>
2019-02-07 12:26:51 -0800

    update

--- node/nextTick.js
+++ node/nextTick.js
@@ -1,4 +1,7 @@
+console.log('Hi!');
 setImmediate(() => console.log('Now'));
+setImmediate(() => console.log('Now 1'));
 process.nextTick(() => console.log('Hello'));
-
+setImmediate(() => console.log('Now 2'));
+setImmediate(() => console.log('Now 3'));
 console.log('world');
